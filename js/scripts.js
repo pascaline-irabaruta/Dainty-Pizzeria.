c44183eb11d5a32395f944afd638c45a4a67b94b
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-10-11 14:53:46 +0200

    create functions to calculate prices

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,61 @@ function Pizza(size, crust) {
             }
       }
       ];
+      var crustPrice = {
+    crispy: 1500,
+    stuffed: 1000,
+    gluten: 1200,
+    neapolitan:2000,
+    Thin:1000
+  };
+  function sizeCalcPrice(size) {
+    if (size === "small") {
+      return sizePrice.small * 1;
+    } else if (size === "medium") {
+      return sizePrice.medium * 1;
+    } else {
+      return sizePrice.large * 1;
+    }
+  }
+
+  function crustCalcPrice(crust) {
+    if (crust === "crispy") {
+      return crustPrice.crispy * 1;
+    } else if (crust === "stuffed") {
+      return crustPrice.stuffed * 1;
+    } else if (crust === "neapolitan") {
+      return crustPrice.neapolitan * 1;
+    } else {
+      return crustPrice.gluten * 1;
+    }
+  }
+
+  function toppingsCalcPrice(toppings) {
+    var noOfTopping = 0;
+    for (i = 0; i < toppings.length; i++) {
+      if (toppings[i] == "Ham") {
+        noOfTopping += 2000;
+      }
+      if (toppings[i] == "Pepperoni") {
+        noOfTopping += 1000;
+      }
+      if (toppings[i] == "Mozzarella") {
+        noOfTopping += 4500;
+      }
+      if (toppings[i] == "Sausage") {
+        noOfTopping += 3000;
+      }
+      if (toppings[i] == "Bacon") {
+        noOfTopping += 4000;
+      }
+      if (toppings[i] == "Mushroom") {
+        noOfTopping += 3500;
+      }
+    }
+    return noOfTopping * 1;
+  }
+
+
+  function checkPepperoni(topping) {
+    return topping === "pepperoni";
+  }
